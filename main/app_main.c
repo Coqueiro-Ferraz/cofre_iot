fb38e261eb720923c98127d915c38f4ddd2a19c6
Hagaceef <121056899+Coqueiro-Ferraz@users.noreply.github.com>
2024-05-23 17:57:17 -0300

    Add files via upload

--- main/app_main.c
+++ main/app_main.c
@@ -6,17 +6,11 @@
 //                                                                                       /              //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/*
-*   Programa básico para controle da placa durante a Jornada da Programação 1
-*   Permite o controle das entradas e saídas digitais, entradas analógicas, display LCD e teclado. 
-*   Cada biblioteca pode ser consultada na respectiva pasta em componentes
-*   Existem algumas imagens e outros documentos na pasta Recursos
-*   O código principal pode ser escrito a partir da linha 86
-*/
 
 // Área de inclusão das bibliotecas
 //-----------------------------------------------------------------------------------------------------------------------
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
@@ -24,13 +18,28 @@
 #include "ioplaca.h"   // Controles das Entradas e Saídas digitais e do teclado
 #include "lcdvia595.h" // Controles do Display LCD
 #include "hcf_adc.h"   // Controles do ADC
-#include "MP_hcf.h"   // Controles do ADC
+#include "MP_hcf.h"   // Controles do Motor
+#include "connect.h"    // Controle do Wifi
+
+
+#include "esp_netif.h"
+#include "mqtt_client.h"
+//#include "protocol_examples_common.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
 
 // Área das macros
 //-----------------------------------------------------------------------------------------------------------------------
 # define PWD 1234
 
-
+#define W_DEVICE_ID "65774aa82623fd911ab650c1" //Use o DeviceID no Wegnology  
+#define W_ACCESS_KEY "76ac5ed2-ed18-4e96-9e02-d2dd572db083" //use a chave de acesso e a senha
+#define W_PASSWORD "f52797619b7205bc2ac8d796d80fd0cb23f988e882cd0b82d575b26939f78c1c"
+#define W_TOPICO_PUBLICAR "wnology/65774aa82623fd911ab650c1/state" //esse número no meio do tópico deve ser mudado pelo ID do seu device Wegnology
+#define W_TOPICO_SUBSCREVER "wnology/65774aa82623fd911ab650c1/command" // aqui também
+#define W_BROKER "mqtt://broker.app.wnology.io:1883"
+#define SSID "coqueiro"
+#define PASSWORD "amigos12"
 // Área de declaração de variáveis e protótipos de funções
 //-----------------------------------------------------------------------------------------------------------------------
 
@@ -42,7 +51,14 @@ int tempo = 50;
 int coluna = 0;
 uint32_t adcvalor = 0;
 char exibir [40];
-// Funções e ramos auxiliares
+char mensa [40];
+const char *strLED = "LED\":"; 
+const char *subtopico_temp = "{\"data\": {\"Temperatura\": ";
+char * Inform;
+bool ledstatus;
+esp_mqtt_client_handle_t cliente;
+int temperatura = 0;
+// Funções e ramos auxiliares para o cofre
 //-----------------------------------------------------------------------------------------------------------------------
 void abrir()
 {
@@ -85,10 +101,132 @@ void restaura()
 {
     senha = 0;
     lcd595_clear();
+    lcd595_write(1,0, " Digite a senha ");
     lcd595_write(2,0, "[ ] [ ] [ ] [ ]");
     controle = 0;
     coluna = 0;
 }
+
+// Funções e ramos auxiliares para o IoT
+//-----------------------------------------------------------------------------------------------------------------------
+void mensagem(esp_mqtt_client_handle_t cliente) 
+{
+   if(strstr(Inform, strLED)) // aqui estou perguntando se o que foi publicado no tópico command está relacionado a minha TAG LED
+   {//caso afirmativo
+        
+       if(strstr(Inform, "true")) // aqui pergunto se a TAG LED recebeu o valor "true"
+       {//se "LED":"true"
+           ledstatus = 1;//inverte LED embarcado
+            /*if(ledstatus==1)//se o LED embarcado está ligado
+            {
+                printf("LED ligado\n");
+                // publico no tópico state {"data":{"est_esp":"link da imagem"}}
+                esp_mqtt_client_publish(cliente, W_TOPICO_PUBLICAR, 
+               // "{\"data\": {\"est_esp\": \"https://files.wnology.io/65774aa82623fd911ab650c1/imagens/esp%20led.png\" }}", 0, 0, 0);
+                "{\"data\": {\"est_esp\": \"https://files.wnology.io/65774aa72623fd911ab650bf/imagens/esp%20led.png\" }}", 0, 0, 0);
+            }
+            else
+            {
+                printf("LED desligado\n");
+                esp_mqtt_client_publish(cliente, W_TOPICO_PUBLICAR, 
+                //"{\"data\": {\"est_esp\": \"https://files.wnology.io/65774aa82623fd911ab650c1/imagens/esp%20ligado.png\" }}", 0, 0, 0);
+                "{\"data\": {\"est_esp\": \"https://files.wnology.io/65774aa72623fd911ab650bf/imagens/esp%20ligado.png\" }}", 0, 0, 0);
+            }*/
+       }
+       else
+       {//se "LED":"false"
+            /*temp_val = rand() % 100;  // valor aleatório entre 0 e 100 para simular a temperatura
+            sprintf(&mensa[0],"%s %d }}",string_temp,temp_val);//isso tudo aqui é só pra mostrar no console o que estou enviando pro broker
+            ESP_LOGI(TAG, "%s", &mensa[0]);//vai aparecer de verde no console: MQTT_EXAMPLE: {"data": {Temperatura": 25 }}
+            esp_mqtt_client_publish(cliente, W_TOPICO_PUBLICAR, &mensa[0], 0, 0, 0); // Aqui é onde está fazendo a publicação no broker efetivamente
+        */
+       
+       }
+        
+       // gpio_set_level(TEC_SH_LD,ledstatus);//aqui é onde realmente acende ou apaga o LED embarcado
+    
+    }
+
+}
+
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0) {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id) {
+
+    case MQTT_EVENT_CONNECTED:        
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        msg_id = esp_mqtt_client_publish(client, W_TOPICO_PUBLICAR,mensa, 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        ESP_LOGI(TAG, "%s", mensa);       
+        msg_id = esp_mqtt_client_subscribe(client, W_TOPICO_SUBSCREVER, 0);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        break;
+
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        Inform = event->data;
+        mensagem(client); 
+        break;
+
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+        }
+        break;
+
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+static void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = W_BROKER,
+        .credentials.set_null_client_id = false,  
+        .credentials.client_id = W_DEVICE_ID,
+        .credentials.username = W_ACCESS_KEY,
+        .credentials.authentication.password = W_PASSWORD,
+        
+    };
+    cliente = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(cliente, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(cliente);
+}
+
 // Programa Principal
 //-----------------------------------------------------------------------------------------------------------------------
 
@@ -102,34 +240,78 @@ void app_main(void)
     ESP_LOGI(TAG, "Versão do IDF: %s", esp_get_idf_version());
 
     /////////////////////////////////////////////////////////////////////////////////////   Inicializações de periféricos (manter assim)
-    
+    ESP_ERROR_CHECK(nvs_flash_init());
+
     // inicializar os IOs e teclado da placa
     ioinit();      
     entradas = io_le_escreve(saidas); // Limpa as saídas e lê o estado das entradas
 
     // inicializar o display LCD 
     lcd595_init();
-    lcd595_write(1,1,"   Jornada 1   ");
-    lcd595_write(2,1,"   COFRE HCF   ");
-    
+    lcd595_write(1,0,"COFRE IOT - v1.0");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);  
+  
+    lcd595_write(1,0,"Inicializando   ");
+    lcd595_write(2,0,"ADC             ");
+
     // Inicializar o componente de leitura de entrada analógica
     esp_err_t init_result = hcf_adc_iniciar();
     if (init_result != ESP_OK) {
         ESP_LOGE("MAIN", "Erro ao inicializar o componente ADC personalizado");
     }
+    
+    lcd595_write(2,0,"ADC / Wifi      ");
+    lcd595_write(1,13,".  ");
+    vTaskDelay(200 / portTICK_PERIOD_MS); 
+    // Inicializar a comunicação IoT
+    wifi_init();    
+    ESP_ERROR_CHECK(wifi_connect_sta(SSID, PASSWORD, 10000));
 
-    //delay inicial
+    lcd595_write(2,0,"C / Wifi / MQTT ");
+    vTaskDelay(100 / portTICK_PERIOD_MS); 
+    lcd595_write(2,0,"Wifi / MQTT     ");
+    lcd595_write(1,13,".. ");
+    vTaskDelay(200 / portTICK_PERIOD_MS); 
+    sprintf(mensa,"%s %d }}",subtopico_temp,temperatura);
+    mqtt_app_start();
+
+    // inicializa driver de motor de passo com fins de curso nas entradas 6 e 7 da placa
+    lcd595_write(2,0,"i / MQTT / DRV  ");
+    vTaskDelay(100 / portTICK_PERIOD_MS); 
+    lcd595_write(2,0,"MQTT / DRV      ");
+    lcd595_write(1,13,"...");
+    vTaskDelay(200 / portTICK_PERIOD_MS); 
+    DRV_init(6,7);
+
+    // fecha a tampa, se estiver aberta
     hcf_adc_ler(&adcvalor);                
     adcvalor = adcvalor*360/4095;
     if(adcvalor>50)fechar();
-    vTaskDelay(1000 / portTICK_PERIOD_MS); 
 
+    lcd595_write(2,0,"TT / DRV / APP  ");
+    vTaskDelay(200 / portTICK_PERIOD_MS); 
+    lcd595_write(2,0,"DRV / APP       ");
+    vTaskDelay(200 / portTICK_PERIOD_MS); 
+    lcd595_write(2,0,"V / APP         ");
+    vTaskDelay(200 / portTICK_PERIOD_MS); 
+    lcd595_write(2,0,"APP             ");
+    //delay inicial para esperar a conexão com internet
+    for(int i=0;i<10;i++)
+    {
+        lcd595_write(1,13,"   ");
+        vTaskDelay(200 / portTICK_PERIOD_MS); 
+        lcd595_write(1,13,".  ");
+        vTaskDelay(200 / portTICK_PERIOD_MS); 
+        lcd595_write(1,13,".. ");
+        vTaskDelay(200 / portTICK_PERIOD_MS); 
+        lcd595_write(1,13,"...");
+        vTaskDelay(200 / portTICK_PERIOD_MS); 
+    }
+    
     lcd595_clear();
     
-    DRV_init(6,7);
     /////////////////////////////////////////////////////////////////////////////////////   Periféricos inicializados
-
-
+    lcd595_write(1,0, " Digite a senha ");
     lcd595_write(2,0, "[ ] [ ] [ ] [ ]");
 
     /////////////////////////////////////////////////////////////////////////////////////   Início do ramo principal                    
@@ -137,8 +319,7 @@ void app_main(void)
     {                                                                                                                                 
         //_______________________________________________________________________________________________________________________________________________________ //
         //-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  - -  -  -  -  -  -  -  -  -  -  Escreva seu código aqui!!! //
-        //int valor = adc1_get_raw(ADC_CHANNEL_0)*360/4095;
-        
+             
         char tecla = le_teclado();
         hcf_adc_ler(&adcvalor);                
         adcvalor = adcvalor*360/4095;
@@ -183,14 +364,15 @@ void app_main(void)
             coluna++;
 
         }
-        if (controle == 4)
+        if (controle == 4 || ledstatus == 1)
         {
-            if (senha == PWD)
+            if (senha == PWD || ledstatus == 1)
             {
                 lcd595_clear();
                 lcd595_write(1,1,"Abrindo...");
                 abrir();
-                controle++;
+                ledstatus = 0;
+                controle=5;
                 lcd595_clear();
                 lcd595_write(1,1,"Aberto    ");
             }
@@ -206,6 +388,7 @@ void app_main(void)
             tempo--;
             sprintf(exibir,"fechar em %d s ", tempo/5);
             lcd595_write(2,0,exibir);
+            
             if(tempo==0) 
             {
                 tempo = 50;
